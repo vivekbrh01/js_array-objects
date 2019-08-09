58cb0037813216d94f85083bb24ce3aa8f4c7acf
nnnkit <iankit17@gmail.com>
2019-08-09 11:38:06 +0530

    add square bracket exercise

---
+++ 5. computed value/index.js
@@ -0,0 +1,14 @@
+// 1. Define a variable named `user` and assign a blank object to it.
+
+// 2. Add a key of `user name` and a value of `Black Panther` to that object.
+
+// 3. Using `console.log` log the value of `user name` key from the user object.
+
+// 4. Add a key of the value of variable `batch` in the object with the value of 10.
+var batch = "myBatch";
+
+// 5. Using the alert function alert the value of the key added above.
+
+// 6. Add a key of `42` to the object with a value of `The answer to the meaning of life 🧸`.
+
+// 7. Using the function console.log log the value of the key `42`.
